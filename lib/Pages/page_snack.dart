4f0c637fd4bb6142fe0da6390b28f495ec6ace1f
Fabien <flierville@cesi.Fr>
2021-09-01 14:34:43 +0200

    Snack

--- lib/Pages/page_snack.dart
+++ lib/Pages/page_snack.dart
@@ -5,6 +5,36 @@ class PageSnack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SNACK"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            callSnack(context);
+          },
+          child: Text("Show SnackBar"),
+        ),
+      ),
+    );
   }
+
+  void callSnack(BuildContext context){
+    SnackBar snackBar = SnackBar(
+        content: Text("Message bien supprimé !"),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.black54,
+      action: SnackBarAction(
+        label: "Annuler",
+        textColor: Colors.lightBlueAccent,
+        onPressed: (){
+          print("Annuler");
+        },
+      ),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
 }
