744b577b5eba00da5648da344d9028cc4d4ad544
Fabien <flierville@cesi.Fr>
2021-09-01 14:21:46 +0200

    Snack

---
+++ lib/Pages/page_snack.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageSnack extends StatelessWidget {
+  const PageSnack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/Pages/page_snack.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PageSnack(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
